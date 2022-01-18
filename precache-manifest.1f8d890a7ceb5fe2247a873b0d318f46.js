e74a1265632d7bb259f211054a3c201a009bfdc8
Ticketer <ticketer@DESKTOP>
2022-01-18 22:20:08 +0700

    Updates

---
+++ precache-manifest.1f8d890a7ceb5fe2247a873b0d318f46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e8f245de6749313b102ac9147f4c103",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1cbfdb2838d7f1273f7b",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "15448a4fa8846c2dd526",
+    "url": "/static/js/2.02d5ec44.chunk.js"
+  },
+  {
+    "revision": "1cbfdb2838d7f1273f7b",
+    "url": "/static/js/main.5735dc62.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
